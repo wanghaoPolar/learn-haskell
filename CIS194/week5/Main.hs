51a2abed3e8bccfe23735d8977ce5d9751fdd6e0
wanghao <wanghaoipv6@gmail.com>
2017-04-10 12:06:10 +0800

    week5 5

--- CIS194/week5/Main.hs
+++ CIS194/week5/Main.hs
@@ -1,14 +1,15 @@
 {-# LANGUAGE TypeSynonymInstances, FlexibleInstances #-}
 module Main where
 
-import Lib
+import Control.Applicative
+import qualified Data.Map as M
 import Expr
 import ExprT
 import ExprParser
 import StackVM
 
 instance Expr ExprT where
-    lit = Lit
+    lit = ExprT.Lit
     add = ExprT.Add
     mul = ExprT.Mul
 
@@ -25,8 +26,8 @@ instance Expr Bool where
 newtype MinMax  = MinMax Integer deriving (Eq, Show)
 instance Expr MinMax where
     lit = MinMax
-    add (MinMax a) (MinMax b) = MinMax (max a b) 
-    mul (MinMax a) (MinMax b) = MinMax (min a b) 
+    add (MinMax a) (MinMax b) = MinMax (max a b)
+    mul (MinMax a) (MinMax b) = MinMax (min a b)
 
 newtype Mod7    = Mod7 Integer deriving (Eq, Show)
 instance Expr Mod7 where
@@ -34,14 +35,13 @@ instance Expr Mod7 where
     add (Mod7 a) (Mod7 b) = Mod7 ((a + b) `mod` 7)
     mul (Mod7 a) (Mod7 b) = Mod7 ((a * b) `mod` 7)
 
-
 eval :: ExprT -> Integer
-eval (Lit a) = a
+eval (ExprT.Lit a) = a
 eval (ExprT.Add a b) = eval a + eval b
 eval (ExprT.Mul a b) = eval a * eval b
 
 evalStr :: String -> Maybe Integer
-evalStr = fmap eval . parseExp Lit ExprT.Add ExprT.Mul 
+evalStr = fmap eval . parseExp ExprT.Lit ExprT.Add ExprT.Mul
 
 reify :: ExprT -> ExprT
 reify = id
@@ -54,5 +54,29 @@ testBool = testExp :: Maybe Bool
 testMM = testExp :: Maybe MinMax
 testSat = testExp :: Maybe Mod7
 
+compile :: String -> Maybe Program
+compile = parseExp lit add mul
+
+-- Simply create an instance of the Expr type class for Program,
+-- so that arithmetic expressions can be interpreted as compiled programs.
+instance Expr Program where
+  lit n = [PushI n]
+  add x y = x ++ y ++ [StackVM.Add]
+  mul x y = x ++ y ++ [StackVM.Mul]
+
+class HasVars a where
+  var :: String -> a
+
+data VarExprT = Lit Integer
+              | Add VarExprT VarExprT
+              | Mul VarExprT VarExprT
+              | Var String Integer
+    deriving (Show, Eq)
+
+instance Expr VarExprT where
+  lit = Main.Lit
+  add = Main.Add
+  mul = Main.Mul
+
 main :: IO ()
-main = someFunc
+main = print (stackVM Control.Applicative.<$> compile "(3 * -4) + 5")
